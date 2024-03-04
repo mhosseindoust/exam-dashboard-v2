326e0aed791d806d16833b8586ee8a3f9e876d98
hamedyousefian <hamed77yosefian@gmail.com>
2024-03-04 12:05:34 +0330

    feat: handle reprocess check bulk scan

--- app/scan/parts/[id]/page.jsx
+++ app/scan/parts/[id]/page.jsx
@@ -3,11 +3,30 @@
 import PageHead from '@/components/PageHead'
 import PageBody from '@/components/PageBody'
 import useSWR from 'swr'
-import { Badge, Button, Spin, Table, Image } from 'antd'
+import { Badge, Button, Spin, Table, Image, App } from 'antd'
 import Link from 'next/link'
+import callAxios from '@/helpers/callAxios'
+import { useState } from 'react'
 
 function Page({ params }) {
-  const { data, isLoading, error } = useSWR(`/exam/scan-part/${params.id}`)
+  const { data, isLoading, error, mutate } = useSWR(`/exam/scan-part/${params.id}`)
+  const [submitLoading, setSubmitLoading] = useState(false)
+  const { message } = App.useApp()
+  const processRecheck = () => {
+    setSubmitLoading(true)
+    callAxios
+      .post(`/exam/scan-part/${params.id}/process`)
+      .then(() => {
+        mutate().then(() => {
+          message.success('با موفقیت انجام شد')
+          setSubmitLoading(false)
+        })
+      })
+      .catch((e) => {
+        message.error(e.errorData.msg)
+        setSubmitLoading(false)
+      })
+  }
   const columns = [
     {
       title: 'Id',
@@ -29,6 +48,10 @@ function Page({ params }) {
           />
         </Badge>
       ),
+    },
+    {
+      title: 'متن',
+      dataIndex: 'json_text',
     },
     // {
     //   title: 'عکس کراپ نشده',
@@ -55,7 +78,11 @@ function Page({ params }) {
   ]
   return (
     <div>
-      <PageHead title='لیست برگه ها' breadcrumbList={[{ title: 'لیست برگه ها' }]} />
+      <PageHead title='لیست برگه ها' breadcrumbList={[{ title: 'لیست برگه ها' }]}>
+        <Button onClick={processRecheck} loading={submitLoading}>
+          بررسی مجدد
+        </Button>
+      </PageHead>
       <PageBody error={error} loading={isLoading}>
         <Table
           size='small'
